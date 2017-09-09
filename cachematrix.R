6f168e78c80e417e7fe9b0de2c8ba2af81cb3a34
sreenip06 <sreeni.prabhala@gmail.com>
2017-09-09 01:05:02 -0500

    Assignment 2 updates

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+################################################################################
+## Below functions provide a way to compute the inverse of a given matrix
+## and cache it so we can avoid unnecessary intense computations to inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix - this function defines a matrix with the given input,
+## and incorporates sun-functions to set and get inverse of the input matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+    # reset inverse in case the matrix input changes
+    xi <- NULL
+    
+    # set - makes a matrix for given input
+    set <- function(y = matrix())
+    {
+        x <<- y
+        xi <<- NULL
+    }
+    
+    # get - returns the input matrix
+    get <- function() x
+    
+    # setsolve - sets the input matrix as the inverse 
+    setsolve <- function(inv = matrix()) xi <<- inv
+    
+    # getsolve - returns the inverse of the matrix
+    getsolve <- function() xi
+    
+    list( set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve - this function checks is the inverse of a given matrix is already 
+## computed; if it is, function reads it from the cache; otherwise, function
+## computes the inverse and caches it
+ 
+cacheSolve <- function(x = matrix(), ...)
+{
+    # check if the inverse is already cached
+    mi <- x$getsolve()
+    if (!is.null(mi))
+    {
+        message("Getting cached inverse")
+        return(mi)
+    }
+    # compute inverse and set it using setsolve
+    m <- x$get()
+    mi <- solve(m, ...)
+    x$setsolve(mi)
+    mi
 }
+################################################################################
